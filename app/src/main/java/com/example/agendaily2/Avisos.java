c521ad71fb01bea809075825af0511a587d1c6e0
= <=>
2022-05-09 22:18:02 +0200

    Diseño 2

--- app/src/main/java/com/example/agendaily2/Avisos.java
+++ app/src/main/java/com/example/agendaily2/Avisos.java
@@ -3,6 +3,7 @@ package com.example.agendaily2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Avisos extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Avisos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avisos);
+    }
+
+    //Metodo mostrar boton volver
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_ayuda, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/agendaily2/Menu.java
+++ app/src/main/java/com/example/agendaily2/Menu.java
@@ -2,7 +2,9 @@ package com.example.agendaily2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void diario(View view){
+
+        Intent diario = new Intent(this, Diario.class);
+        startActivity(diario);
+        finish();
+
+    }
+    public void notas(View view){
+
+        Intent notas = new Intent(this, Notas.class);
+        startActivity(notas);
+        finish();
+
+    }
+    public void avisos(View view){
+
+        Intent avisos = new Intent(this, Avisos.class);
+        startActivity(avisos);
+        finish();
+
+    }
+    public void calendario(View view){
+
+        Intent calendario = new Intent(this, Calendario.class);
+        startActivity(calendario);
+        finish();
+
     }
 }
